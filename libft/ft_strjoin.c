c25958bf4a702b931d1fae969cc8c6163e9c1e8a
system <youpark@students.42seoul.kr>
2020-10-10 12:34:16 +0900

    Change directory

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -1,31 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: youpark <youpark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/07 19:05:43 by youpark           #+#    #+#             */
-/*   Updated: 2020/10/07 19:08:01 by youpark          ###   ########.fr       */
+/*   Created: 2020/10/07 20:01:06 by youpark           #+#    #+#             */
+/*   Updated: 2020/10/10 12:33:46 by youpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	unsigned int	cnt;
-	unsigned int	i;
+	char	*str;
+	int		len1;
+	int		len2;
 
-	cnt = 0;
-	while (src[cnt] != '\0')
-		++cnt;
-	i = 0;
-	while (src[i] != '\0' && i < (size - 1))
-	{
-		dst[i] = src[i];
-		++i;
-	}
-	dst[i] = '\0';
-	return (cnt);
+	if (!(s1) && !(s2))
+		return (0);
+	if (!(s1))
+		return (ft_strdup(s2));
+	else if (!(s2))
+		return (ft_strdup(s1));
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	if (!(str = (char *)malloc(sizeof(char) * (len1 + len2 + 1))))
+		return (0);
+	ft_strlcpy(str, s1, len1 + 1);
+	ft_strlcpy(str + len1, s2, len2 + 1);
+	return (str);
 }
--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -1,34 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: youpark <youpark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/07 20:01:06 by youpark           #+#    #+#             */
-/*   Updated: 2020/10/07 21:24:10 by youpark          ###   ########.fr       */
+/*   Created: 2020/10/07 19:05:43 by youpark           #+#    #+#             */
+/*   Updated: 2020/10/10 12:32:59 by youpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	char	*str;
-	int		len1;
-	int		len2;
+	unsigned int	cnt;
+	unsigned int	i;
 
-	if (!(s1) && !(s2))
-		return (0);
-	if (!(s1))
-		return (ft_strdup(s2));
-	else
-		return (ft_strdup(s1));
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
-	if (!(str = (char *)malloc(sizeof(char) * (len1 + len2 + 1))))
-		return (0);
-	ft_strlcpy(str, s1, len1 + 1);
-	ft_strlcpy(str + len1, s2, len2 + 1);
-	return (str);
+	cnt = 0;
+	while (src[cnt] != '\0')
+		++cnt;
+	i = 0;
+	if (size)
+	{
+		while (src[i] != '\0' && i < (size - 1))
+		{
+			dst[i] = src[i];
+			++i;
+		}
+		dst[i] = '\0';
+	}
+	return (cnt);
 }
